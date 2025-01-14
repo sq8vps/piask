a06ecb0d976d91a0311ccee0ec17e29f04a9c61e
sq8vps <sq8vps@gmail.com>
2025-01-14 12:12:28 +0100

    working on cuda

--- config.h
+++ config.h
@@ -1,18 +1,33 @@
 #ifndef CONFIG_H_
 #define CONFIG_H_
 
+#if !defined(USE_PLAIN) && !defined(USE_OPENMP) && !defined(USE_MPI) && !defined(USE_CUDA)
+
 //optimization method to use
 //define here or using -D in compiler command line
 //#define USE_PLAIN
 //#define USE_OPENMP
 //#define USE_MPI
+#define USE_CUDA
 
-#define NUM_THREADS 4 //number of threads to be used
+#endif
+
+#if !defined(NUM_BLOCKS)
+#define NUM_BLOCKS 4 //number of thread blocks (CUDA)
+#endif
+
+#if !defined(NUM_THREADS)
+#define NUM_THREADS 4 //number of threads to be used (OpenMP, CUDA)
+#endif
+
+#if !defined(USE_FLOAT) && !defined(USE_DOUBLE) && !defined(USE_LONG_DOUBLE)
 
 //type to use for real numbers
 #define USE_FLOAT
 //#define USE_DOUBLE
 //#define USE_LONG_DOUBLE
+
+#endif
 
 #define NUM_PARTICLES 10000000 //number of particles
 #define CENTER {0.L, 0.L, 0.L} //center point
@@ -21,7 +36,7 @@
 #define RANDOM_LIMIT (REAL[2]){-10.L, 10.L} //random coordinates generation lower and higher limit
 
 
-#if !(defined(USE_PLAIN) ^ defined(USE_OPENMP) ^ defined(USE_MPI))
+#if !(defined(USE_PLAIN) ^ defined(USE_OPENMP) ^ defined(USE_MPI) ^ defined(USE_CUDA))
 #error One and only one optimization method might be used
 #endif
 
---
+++ cuda.c
@@ -0,0 +1,83 @@
+#include "algo.h"
+#include "config.h"
+
+#ifdef USE_CUDA
+
+#define PARTICLES_PER_THREAD (NUM_PARTICLES / (NUM_BLOCKS * NUM_THREADS))
+
+static Particle *cudaInput = NULL;
+static Particle **cudaOutput = NULL;
+__shared__ int sum = 0;
+
+__global__ void cudaGenerate(void)
+{
+    int index = threadIdx.x + blockIdx.x * blockDim.x;
+    size_t shift = index * PARTICLES_PER_THREAD;
+
+    for(size_t i = 0; i < PARTICLES_PER_THREAD; i++)
+    {
+        cudaInput[shift + i].x = randr();
+        cudaInput[shift + i].y = randr();
+        cudaInput[shift + i].z = randr();
+    }
+}
+
+__global__ void cudaFind(void)
+{
+    Vec3 center = CENTER;
+    int index = threadIdx.x + blockIdx.x * blockDim.x;
+    size_t shift = index * PARTICLES_PER_THREAD;
+
+    int count = 0;
+
+    for(size_t i = 0; i < PARTICLES_PER_THREAD; i++)
+    {
+        if(ABS(distance(&(cudaInput[shift + i]), &center) - DISTANCE) <= DELTA)
+        {
+            cudaOutput[shift + count++] = &(cudaInput[shift + i]);
+        }
+    }
+
+    atomicAdd(&sum, count);
+}
+
+bool Generate(Particle *buffer, void *context)
+{
+    cudaMalloc((void**)&cudaInput, NUM_PARTICLES * sizeof(Particle));
+
+    cudaGenerate<<<NUM_BLOCKS, NUM_THREADS>>>();
+
+    cudaMemcpy(buffer, cudaInput, NUM_PARTICLES * sizeof(Particle), cudaMemcpyDeviceToHost);
+
+    return true;
+}
+
+size_t Find(Particle *input, Particle **output, void *context)
+{
+    cudaMalloc((void***)&cudaOutput, NUM_PARTICLES * sizeof(Particle*));
+    cudaMalloc((void**)&cudaCount, NUM_THREADS * NUM_BLOCKS * sizeof(size_t*));
+
+    cudaFind<<<NUM_BLOCKS, NUM_THREADS>>>();
+
+    cudaMemcpy(output, cudaOutput, NUM_PARTICLES * sizeof(Particle*));
+    cudaFree(cudaOutput);
+    cudaFree(cudaInput);
+
+    return sum;
+}
+
+bool Initialize(int argc, char **argv, void **context)
+{
+    return true;
+}
+
+void Finalize(void *context)
+{
+    
+}
+
+#if (NUM_PARTICLES % (NUM_BLOCKS * NUM_THREADS))
+    #error Number of particles must be divisible by the number of blocks times the number of threads
+#endif
+
+#endif
